bb1462eb3d3a751e5eca9bcbf57714692d267c47
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2024-10-23 21:00:56 +0700

    core: added sessions

---
+++ app/core/session/auth/auth.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"github.com/gorilla/sessions"
+	"github.com/novaru/go-shop/app/models"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"net/http"
+	"os"
+)
+
+var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+var sessionUser = "user-session"
+
+func GetSessionUser(r *http.Request) (*sessions.Session, error) {
+	return store.Get(r, sessionUser)
+}
+
+func IsLoggedIn(r *http.Request) bool {
+	session, _ := store.Get(r, sessionUser)
+	return session.Values["id"] != nil
+}
+
+func ComparePassword(password string, hashedPassword string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)) == nil
+}
+
+func MakePassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	return string(hashedPassword), err
+}
+
+func CurrentUser(db *gorm.DB, w http.ResponseWriter, r *http.Request) *models.User {
+	if !IsLoggedIn(r) {
+		return nil
+	}
+
+	session, _ := store.Get(r, sessionUser)
+
+	userModel := models.User{}
+	user, err := userModel.FindByID(db, session.Values["id"].(string))
+	if err != nil {
+		session.Values["id"] = nil
+		session.Save(r, w)
+		return nil
+	}
+
+	return user
+}
--- go.mod
+++ go.mod
@@ -18,6 +18,8 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.4.0 // indirect
 	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
