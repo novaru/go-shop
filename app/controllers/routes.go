7e50844dcef960d010aa9abd0f9cc3bb74920fa4
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2024-10-24 11:31:18 +0700

    feat: support update cart and calculate shipping
    Signed-off-by: Febriyan Ahza <69071485+novaru@users.noreply.github.com>

--- app/controllers/routes.go
+++ app/controllers/routes.go
@@ -13,6 +13,9 @@ func (server *Server) InitializeRoutes() {
 
 	server.Router.HandleFunc("/carts", server.GetCart).Methods("GET")
 	server.Router.HandleFunc("/carts", server.AddItemToCart).Methods("POST")
+	server.Router.HandleFunc("/carts/update", server.UpdateCart).Methods("POST")
+	server.Router.HandleFunc("/carts/cities", server.GetCitiesByProvince).Methods("GET")
+	server.Router.HandleFunc("/carts/remove/{id}", server.RemoveItemByID).Methods("GET")
 
 	staticFileDirectory := http.Dir("./assets/")
 	staticFileHandler := http.StripPrefix("/public/", http.FileServer(staticFileDirectory))
