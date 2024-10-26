42bd85fb4a2f33e73980b9988dfdde0201843ff4
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2024-10-27 00:25:02 +0700

    core: added checkout

--- app/models/order_item.go
+++ app/models/order_item.go
@@ -1,4 +1,39 @@
 package models
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+
+	"github.com/shopspring/decimal"
+)
+
 type OrderItem struct {
+	ID              string `gorm:"size:36;not null;uniqueIndex;primary_key"`
+	Order           Order
+	OrderID         string `gorm:"size:36;index"`
+	Product         Product
+	ProductID       string `gorm:"size:36;index"`
+	Qty             int
+	BasePrice       int
+	BaseTotal       int
+	TaxAmount       decimal.Decimal `gorm:"type:decimal(16,2)"`
+	TaxPercent      decimal.Decimal `gorm:"type:decimal(10,2)"`
+	DiscountAmount  decimal.Decimal `gorm:"type:decimal(16,2)"`
+	DiscountPercent decimal.Decimal `gorm:"type:decimal(10,2)"`
+	SubTotal        int
+	Sku             string          `gorm:"size:36;index"`
+	Name            string          `gorm:"size:255"`
+	Weight          decimal.Decimal `gorm:"type:decimal(10,2)"`
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+func (o *OrderItem) BeforeCreate(db *gorm.DB) error {
+	if o.ID == "" {
+		o.ID = uuid.New().String()
+	}
+
+	return nil
 }
